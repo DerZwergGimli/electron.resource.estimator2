486ce7a8d44557ec26d06726231abef0c2ca0b2b
DZWG <paule22@protonmail.com>
2023-01-17 17:13:28 +0100

    updated base content view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="home flex flex-col text-center">
+  <div class="home flex flex-col text-center base-content">
+    <button class="btn-style m-3" @click="clear_local_storage">
+      Reset App-Data
+    </button>
+
     <div class="flex flex-row justify-center space-x-2">
       <p class="basis-1/2">
         This tool is used for estimating hardware resources for virtual
         environments.
       </p>
     </div>
-
-    <button class="btn-style m-3" @click="clear_local_storage">
-      Reset App-Data
-    </button>
   </div>
 </template>
 
