0f53f0570664fec82e78a192f5996586876900c4
DZWG <paule22@protonmail.com>
2023-01-24 14:51:07 +0100

    added button for uploading preset and added filetypes

--- src/views/ImportView.vue
+++ src/views/ImportView.vue
@@ -1,14 +1,40 @@
 <template>
-  <div class="flex flex-col text-center">
-    <div class="base-content">
-      <input type="file" @change="evt_uploadFile" />
+  <div class="flex flex-col">
+    <div class="base-content flex flex-col space-y-3">
+      <div class="flex flex-col items-center bg-gray-700 m-2 p-2 rounded-md">
+        <h2>Import Config-File</h2>
+        <Button color="default">
+          <input
+            class="input-group"
+            type="file"
+            accept=".json"
+            @change="evt_uploadFile"
+        /></Button>
+      </div>
+      <div class="flex flex-col items-center bg-gray-700 m-2 p-2 rounded-md">
+        <h2>Import Preset-File</h2>
+
+        <Button color="default">
+          <input
+            class="input-group"
+            type="file"
+            accept=".json"
+            @change="() => createToast('not implemented', TOAST_WARNING)"
+        /></Button>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { Button } from 'flowbite-vue'
 import { createToast } from 'mosha-vue-toastify'
 import { useAppStorage } from '../store/AppStorage'
+import {
+  TOAST_ERROR,
+  TOAST_SUCCESS,
+  TOAST_WARNING,
+} from '../extra/toast-config'
 
 const storage = useAppStorage()
 
@@ -23,14 +49,12 @@ function evt_uploadFile(event: any) {
     const data_string = reader.result?.toString()
     const data_json = JSON.parse(JSON.parse(JSON.stringify(data_string)))
     storage.import(data_json.hosts, data_json.vms, data_json.assignments)
-    createToast('Data imported from File', { type: 'success' })
+    createToast('Data imported from File', TOAST_SUCCESS)
     // console.info("...Imported!");
   }
 
   reader.onerror = function () {
-    createToast('Error loading file content (check console)', {
-      type: 'danger',
-    })
+    createToast('Error loading file content (check console)', TOAST_ERROR)
     console.error(reader.error)
   }
 
